e5328381c03fade129344a92430eb322c2081aa9
moinul <moinul@moinuls-MacBook-Pro.local>
2023-05-16 12:04:23 +0600

    project initialize

---
+++ src/main/java/com/aang13/springboottfa/SpringBootTfaApplication.java
@@ -0,0 +1,13 @@
+package com.aang13.springboottfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aang13/springboottfa/SpringBootTfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aang13.springboottfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
